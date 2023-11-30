4aea1553e0418eea5b3cd985833862d8bc655459
Tilen Komel <tilen.komel10@gmail.com>
2023-11-30 18:26:19 +0100

    Add widget course card

--- podjetni-elementor.php
+++ podjetni-elementor.php
@@ -11,3 +11,29 @@ if (!defined('ABSPATH')) exit;
  * Text Domain: podjetni-elementor
  * License: GPLv3
  */
+
+ /* -----------------------------------------------------------------------------
+ Register Podjetni categories.
+----------------------------------------------------------------------------- */
+function register_custom_elementor_categories($elements_manager) {
+  $elements_manager->add_category(
+      'podjetni',
+      [
+          'title' => esc_html__('Podjetni', 'podjetni-elementor'),
+          'icon' => 'fa fa-plug',
+      ]
+  );
+}
+add_action('elementor/elements/categories_registered', 'register_custom_elementor_categories');
+
+
+
+/* -----------------------------------------------------------------------------
+Register Podjetni widgets.
+----------------------------------------------------------------------------- */
+function register_custom_elementor_widgets($widgets_manager) {
+  require_once(__DIR__ . '/widgets/course-card.php');
+
+  $widgets_manager->register(new \Elementor_Podjetni_Course_Card());
+}
+add_action('elementor/widgets/register', 'register_custom_elementor_widgets');
