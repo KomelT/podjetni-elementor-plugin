0fb1076b7e46a24cf3027c0387a946c42dc8fda1
Tilen Komel <tilen.komel10@gmail.com>
2023-11-30 18:15:04 +0100

    Add plugin guts

---
+++ podjetni-elementor.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+/**
+ * Plugin Name: Podjetni Elementor
+ * Description: Elementor widgets for Podjetni.si
+ * Plugin URI:  https://github.com/KomelT/podjetni-elementor-plugin
+ * Version:     1.0.0
+ * Author:      Tilen Komel
+ * Author URI:  https://www.komelt.dev/
+ * Text Domain: podjetni-elementor
+ * License: GPLv3
+ */
